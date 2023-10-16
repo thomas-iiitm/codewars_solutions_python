e87913c6b09059ff19b93766f085f188fb214665
Thomas <thomas.iiitm@gmail.com>
2023-10-16 13:57:53 +0530

    convert underscore hyphen separated words to camelcase

---
+++ convertStringToCamelCase.py
@@ -0,0 +1,10 @@
+# https://www.codewars.com/kata/517abf86da9663f1d2000003/python
+
+def to_camel_case(text):
+    temp = text.replace('-','_')
+    temp = temp.split('_')
+    res = []
+    res.append(temp[0])
+    for i in range(1,len(temp)):
+        res.append(temp[i].capitalize())
+    return ''.join(res)
